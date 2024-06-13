5de766a0d9838167e9f995665b15e3a995f86fef
heldaaryuputri <162781938+heldaaryuputri@users.noreply.github.com>
2024-06-13 15:21:28 +0700

    Add files via upload

---
+++ todo_list_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_list_provider/Providers/todo_provider.dart';
+import 'package:todo_list_provider/Widgets/todo_item.dart';
+
+
+class TodoListScreen extends StatelessWidget {
+
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Todo List'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _controller,
+                    decoration: InputDecoration(
+                      labelText: 'Add Todo',
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.add),
+                  onPressed: () {
+                    if (_controller.text.isNotEmpty) {
+                      Provider.of<TodoProvider>(context, listen: false)
+                          .addTodo(_controller.text);
+                      _controller.clear();
+                    }
+                  },
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Consumer<TodoProvider>(
+              builder: (context, todoProvider, child) {
+                return ListView.builder(
+                  itemCount: todoProvider.todos.length,
+                  itemBuilder: (context, index) {
+                    final todo = todoProvider.todos[index];
+                    return TodoItem(todo: todo);
+                  },
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
